2704753aa591e57dda6ff2db9b86184fb24d0035
ale93ag <alessandragalotta93@yahoo.com>
2023-10-01 09:08:06 +0200

    Aggiunto modulo fecth per dividere il codice in più funzioni

--- src/JS/fetch.js
+++ src/JS/fetch.js
@@ -1,99 +1,12 @@
-import { formatCityName } from "./formatCityName.js";
+// fetchData.js
 
-let city = "";
-
-// Dichiarazione delle variabili
-const summary = document.querySelector(".summary");
-const category = document.querySelector(".category");
-const input = document.querySelector("input");
-const score = document.querySelector(".score");
-const nameScore = document.querySelector(".nameScore");
-const btn = document.querySelector("button");
-const main = document.querySelector("main");
-
-let clearCard = function () {
-  category.innerHTML = "";
-  nameScore.innerHTML = "";
-  score.innerHTML = "";
-  score.classList.remove("pulse"); // Rimuovi la classe 'pulse' per interrompere l'animazione
-};
-
-// Funzione per ottenere i dati tramite API
-const getData = async function () {
-  const getScore = await fetch(
-    `https://api.teleport.org/api/urban_areas/slug:${city}/scores/`
-  );
-
-  const dataScore = await getScore.json();
-
-  // DOM MANIPULATION
-  if (getScore.status === 200) {
-    summary.innerHTML = `<h2><p>${dataScore.summary}</p></h2>`;
-    category.innerHTML = "";
-    nameScore.innerHTML = "CITY SCORE";
-    score.innerHTML = dataScore.teleport_city_score.toFixed(2);
-    score.classList.add("pulse"); // Aggiungi la classe 'pulse' per l'animazione
-
-    dataScore.categories.forEach((x) => {
-      category.insertAdjacentHTML(
-        "afterbegin",
-        `<h3><span class="name">${x.name}</span> <span class="scores">${x.score_out_of_10.toFixed(
-          1
-        )}</span></h3>`
-      );
-
-      const containerAllElement = document.createElement("div");
-      containerAllElement.classList.add("containerAllElement");
-      const divScore = document.createElement("div");
-      divScore.classList.add("divScore");
-      divScore.appendChild(nameScore);
-      divScore.appendChild(score);
-      const descriptionElement = document.createElement("div");
-      descriptionElement.classList.add("descriptionElement");
-      descriptionElement.appendChild(summary);
-      descriptionElement.appendChild(category);
-
-      containerAllElement.appendChild(descriptionElement);
-      containerAllElement.appendChild(divScore);
-
-      main.appendChild(containerAllElement);
-    });
-  } else {
-    errorCatching(
-      summary,
-      `<h3>Città non trovata. Controlla eventuali errori di battitura. <br> Ricorda di scrivere il nome della città in inglese.<br>  Se nessuno di questi problemi è presente, forse la città non è presente nel nostro database..<h3>`
+export async function getData(city) {
+    const getScore = await fetch(
+      `https://api.teleport.org/api/urban_areas/slug:${city}/scores/`
     );
-    clearCard();
-  }
-};
-
-// Funzioni per controllare gli errori
-const errorCatching = (summary, warningMessage) => {
-  summary.innerHTML = `<p>${warningMessage}</p>`;
-  return warningMessage;
-};
-
-const errorVoid = () => {
-  if (!input.value) {
-    errorCatching(
-      summary,
-      `<h1>SCRIVI UNA CITTA'</h1>`
-    );
-    clearCard();
-  }
-};
-
-btn.addEventListener("click", function () {
-  city = formatCityName(input.value);
-  errorVoid(input);
-  getData();
-});
-
-
-
-
-
-
   
-
-
+    const dataScore = await getScore.json();
+  
+    return dataScore;
+  }
+  
